dc21cd9b21d62a9893ce7294977a11fece8cdaff
Keenan Graham <ksgraham@stanford.edu>
2022-10-20 16:24:43 -0700

    OS deployed

--- app.py
+++ app.py
@@ -1,28 +1,82 @@
-#!/usr/bin/env python3
-import os
+from aws_cdk import App
+from aws_cdk import Stack
+from aws_cdk import RemovalPolicy
 
-import aws_cdk as cdk
+from constructs import Construct
 
-from cdk_opensearch_queue.cdk_opensearch_queue_stack import CdkOpensearchQueueStack
+from shared_infrastructure.cherry_lab.environments import US_WEST_2
+from shared_infrastructure.cherry_lab.vpcs import VPCs
+
+from aws_cdk.aws_opensearchservice import Domain
+from aws_cdk.aws_opensearchservice import EngineVersion
+from aws_cdk.aws_opensearchservice import CapacityConfig
+from aws_cdk.aws_opensearchservice import EbsOptions
+from aws_cdk.aws_opensearchservice import LoggingOptions
+
+from aws_cdk.aws_ec2 import SubnetSelection
+from aws_cdk.aws_ec2 import SubnetType
 
 
-app = cdk.App()
-CdkOpensearchQueueStack(app, "CdkOpensearchQueueStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+class Opensearch(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        vpcs = VPCs(
+            self,
+            'VPCs'
+        )
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+        domain = Domain(
+            self,
+            'Domain',
+            version=EngineVersion.OPENSEARCH_1_2,
+            capacity=CapacityConfig(
+                data_node_instance_type='t3.small.search',
+                data_nodes=1,
+            ),
+            ebs=EbsOptions(
+                volume_size=10,
+            ),
+            logging=LoggingOptions(
+                app_log_enabled=True,
+                slow_index_log_enabled=True,
+                slow_search_log_enabled=True,
+            ),
+            removal_policy=RemovalPolicy.DESTROY,
+            vpc=vpcs.default_vpc,
+            vpc_subnets=[
+                SubnetSelection(
+                    availability_zones=['us-west-2a'],
+                    subnet_type=SubnetType.PUBLIC,
+                ),
+            ],
+            advanced_options={
+                'indices.query.bool.max_clause_count': '8096'
+            }
+        )
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+        domain.app_log_group.apply_removal_policy(
+            RemovalPolicy.DESTROY
+        )
+        domain.slow_index_log_group.apply_removal_policy(
+            RemovalPolicy.DESTROY
+        )
+        domain.slow_search_log_group.apply_removal_policy(
+            RemovalPolicy.DESTROY
+        )
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+app = App()
+
+
+Opensearch(
+    app,
+    'OpensearchStack',
+    env=US_WEST_2,
+)
+
+
+# 'r5.xlarge'
 
 app.synth()
